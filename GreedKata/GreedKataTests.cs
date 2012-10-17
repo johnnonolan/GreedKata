d5f4adfa9622b996a9f4998cde1123b389847aeb
John Nolan <john.nolan@moneyplusgroup.co.uk>
2012-10-17 13:44:19 +0100

    Added in a dice throwing engine so test more sensible

--- GreedKata/GreedKataTests.cs
+++ GreedKata/GreedKataTests.cs
@@ -3,21 +3,38 @@
 namespace GreedKata
 {
     [TestFixture]
-    public class GreedKataTests
+    public class GreedKataTests : IRoller
     {
         [Test]
         public void When_throwing_a_single_five_you_score_50()
         {
-            var game = new Game();
+            
+            var game = new Game(this);
             game.throwDice();
             var score = game.Score();
             Assert.That(score ,Is.EqualTo(50));
         }
 
+        public int[] Roll()
+        {
+            return new[] {2, 3, 4, 6, 5};
+        }
+    }
+
+    public interface IRoller
+    {
+        int[] Roll();
     }
 
     public class Game
     {
+        readonly IRoller _roller;
+
+        public Game(IRoller roller)
+        {
+            _roller = roller;
+        }
+
         public int Score()
         {
             return 50;
@@ -25,7 +42,7 @@ namespace GreedKata
 
         public void throwDice()
         {
-
+            var dice = _roller.Roll();
         }
     }
 }
