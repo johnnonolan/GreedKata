32880c506f2c55897c3a71d0ca7ed547ba873b05
John Nolan <john.nolan@moneyplusgroup.co.uk>
2012-10-17 13:37:15 +0100

    initial commit
    1st test passed

---
+++ GreedKata/GreedKataTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace GreedKata
+{
+    [TestFixture]
+    public class GreedKataTests
+    {
+        [Test]
+        public void When_throwing_a_single_five_you_score_50()
+        {
+            var game = new Game();
+            game.throwDice();
+            var score = game.Score();
+            Assert.That(score ,Is.EqualTo(50));
+        }
+
+    }
+
+    public class Game
+    {
+        public int Score()
+        {
+            return 50;
+        }
+
+        public void throwDice()
+        {
+
+        }
+    }
+}
